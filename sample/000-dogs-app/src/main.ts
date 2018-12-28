c3cf3b9977e8c2144cdda0dae58c84704db6d8b2
Livio <livio.brunner.lb1@gmail.com>
2018-12-28 12:10:05 +0100

    fix(bootsrap): Use appRef.instance.server for fastify httpServer
    Fixes #24

--- sample/000-dogs-app/src/main.ts
+++ sample/000-dogs-app/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule);
+  const app = await NestFactory.create(ApplicationModule, new FastifyAdapter());
   await app.listen(3000);
 }
 bootstrap();
