085e08824aa8cb043a9cf5bb1d8e86479ca8259c
Livio <livio.brunner.lb1@gmail.com>
2019-04-07 23:26:09 +0200

    sample(003): Add memory sample app

---
+++ sample/003-memory-app/src/health/terminus-options.service.ts
@@ -0,0 +1,26 @@
+import {
+  TerminusOptionsFactory,
+  TerminusEndpoint,
+  TerminusModuleOptions,
+  DiskHealthIndicator,
+  MemoryHealthIndicator,
+} from '../../../../lib';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TerminusOptionsService implements TerminusOptionsFactory {
+  constructor(private readonly memory: MemoryHealthIndicator) {}
+
+  public createTerminusOptions(): TerminusModuleOptions {
+    const healthEndpoint: TerminusEndpoint = {
+      url: '/health',
+      healthIndicators: [
+        async () => this.memory.checkHeap('memory_heap', 200 * 1024 * 1024),
+        async () => this.memory.checkRSS('memory_rss', 3000 * 1024 * 1024),
+      ],
+    };
+    return {
+      endpoints: [healthEndpoint],
+    };
+  }
+}
