bcd6a50466e56b3e2ce0295fae4368ada2f233a3
Livio <livio.brunner.lb1@gmail.com>
2019-04-07 23:14:56 +0200

    test(memory): Update e2e test with checkRss

--- e2e/health-checks/memory.health.e2e-spec.ts
+++ e2e/health-checks/memory.health.e2e-spec.ts
@@ -7,64 +7,96 @@ import { bootstrapModule } from '../helper/bootstrap-module';
 describe('Memory Health', () => {
   let app: INestApplication;
   let port: number;
-  let getTerminusOptions: (
-    disk: MemoryHealthIndicator,
-  ) => TerminusModuleOptions;
-  beforeEach(async () => {
-    getTerminusOptions = (
-      disk: MemoryHealthIndicator,
-    ): TerminusModuleOptions => ({
-      endpoints: [
-        {
-          url: '/health',
-          healthIndicators: [
-            async () => {
-              const { heapUsed } = process.memoryUsage();
-              return disk.check('memory', { threshold: heapUsed + 1 });
-            },
-          ],
-        },
-      ],
-    });
-  });
 
-  it('should check if the memory threshold is not exceeded', async () => {
-    [app, port] = await bootstrapModule({
-      inject: [MemoryHealthIndicator],
-      useFactory: getTerminusOptions,
-    });
-    const response = await Axios.get(`http://0.0.0.0:${port}/health`);
-    expect(response.status).toBe(200);
-    expect(response.data).toEqual({
-      status: 'ok',
-      info: { memory: { status: 'up' } },
-    });
-  });
-
-  it('should check if correctly displays a threshold exceeded error', async () => {
-    [app, port] = await bootstrapModule({
-      inject: [MemoryHealthIndicator],
-      useFactory: (disk: MemoryHealthIndicator): TerminusModuleOptions => ({
+  describe('checkRss', () => {
+    let getTerminusOptions: (
+      memory: MemoryHealthIndicator,
+    ) => TerminusModuleOptions;
+    beforeEach(async () => {
+      getTerminusOptions = (
+        memory: MemoryHealthIndicator,
+      ): TerminusModuleOptions => ({
         endpoints: [
           {
             url: '/health',
             healthIndicators: [
-              async () => disk.check('disk', { threshold: 0 }),
+              async () => {
+                const { rss } = process.memoryUsage();
+                return memory.checkHeap('memory_rss', rss + 1);
+              },
             ],
           },
         ],
-      }),
+      });
     });
 
-    try {
-      await Axios.get(`http://0.0.0.0:${port}/health`);
-    } catch (error) {
-      expect(error.response.status).toBe(503);
-      expect(error.response.data).toEqual({
-        status: 'error',
-        error: { disk: { status: 'down', message: expect.any(String) } },
+    it('should check if the rss threshold is not exceeded', async () => {
+      [app, port] = await bootstrapModule({
+        inject: [MemoryHealthIndicator],
+        useFactory: getTerminusOptions,
       });
-    }
+      const response = await Axios.get(`http://0.0.0.0:${port}/health`);
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual({
+        status: 'ok',
+        info: { memory_rss: { status: 'up' } },
+      });
+    });
+  });
+
+  describe('checkHeap', () => {
+    it('should check if the heap threshold is not exceeded', async () => {
+      const getTerminusOptions = (
+        memory: MemoryHealthIndicator,
+      ): TerminusModuleOptions => ({
+        endpoints: [
+          {
+            url: '/health',
+            healthIndicators: [
+              async () =>
+                memory.checkHeap('memory_heap', 1 * 1024 * 1024 * 1024 * 1024),
+            ],
+          },
+        ],
+      });
+
+      [app, port] = await bootstrapModule({
+        inject: [MemoryHealthIndicator],
+        useFactory: getTerminusOptions,
+      });
+      const response = await Axios.get(`http://0.0.0.0:${port}/health`);
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual({
+        status: 'ok',
+        info: { memory_heap: { status: 'up' } },
+      });
+    });
+
+    it('should check if correctly displays a heap exceeded error', async () => {
+      [app, port] = await bootstrapModule({
+        inject: [MemoryHealthIndicator],
+        useFactory: (disk: MemoryHealthIndicator): TerminusModuleOptions => ({
+          endpoints: [
+            {
+              url: '/health',
+              healthIndicators: [async () => disk.checkHeap('memory_heap', 0)],
+            },
+          ],
+        }),
+      });
+
+      try {
+        await Axios.get(`http://0.0.0.0:${port}/health`);
+      } catch (error) {
+        expect(error.response.status).toBe(503);
+        expect(error.response.data).toEqual({
+          status: 'error',
+          error: {
+            memory_heap: { status: 'down', message: expect.any(String) },
+          },
+        });
+      }
+    });
   });
 
   afterEach(async () => await app.close());
