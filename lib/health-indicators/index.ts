179db8b4bb272d18c58d9dfc2c71bca4b59325a4
Livio <livio.brunner.lb1@gmail.com>
2019-05-21 16:24:19 +0200

    feat(@nestjs/terminus): Add grpc health indicator

--- lib/health-indicators/index.ts
+++ lib/health-indicators/index.ts
@@ -2,6 +2,7 @@ export * from './dns/dns.health';
 export * from './database/mongoose.health';
 export * from './database/typeorm.health';
 export * from './microservice/microservice.health';
+export * from './microservice/grpc.health';
 export * from './disk';
 export * from './memory';
 
--- lib/utils/types.ts
+++ lib/utils/types.ts
@@ -8,3 +8,8 @@ export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
 export type XOR<T, U> = (T | U) extends object
   ? (Without<T, U> & U) | (Without<U, T> & T)
   : T | U;
+
+/**
+ * @internal
+ */
+export type PropType<TObj, TProp extends keyof TObj> = TObj[TProp];
