60b411e758fc70e16471ba45f2d24feab588b6de
Tony133 <a.tripodi133@gmail.com>
2020-10-28 11:28:17 +0100

    Added sample 002-microservice-app with use v7 API

---
+++ sample/002-microservice-app/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 8889,
+    },
+  });
+  await app.startAllMicroservicesAsync();
+  await app.listen(3000);
+}
+bootstrap();
