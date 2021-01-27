1fd7b9528aa89e9ed48ea3bc2ae19c5581158dd0
Tony133 <a.tripodi133@gmail.com>
2021-01-27 22:53:15 +0100

    chroe(sample-000): fix indentation

--- sample/000-dogs-app/src/health/health.module.ts
+++ sample/000-dogs-app/src/health/health.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
-
 import { DogModule } from '../dog/dog.module';
 import { HealthController } from './health.controller';
 
