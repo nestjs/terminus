e973f234b7f50271c64bb1391bd58e6b95da458a
Tony133 <a.tripodi133@gmail.com>
2021-03-23 17:52:29 +0100

    chore(): updated gulp configurations

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,107 +1,16 @@
-// @ts-check
+'use strict';
+/**
+ * Load the TypeScript compiler, then load the TypeScript gulpfile which simply loads all
+ * the tasks. The tasks are really inside tools/gulp/tasks.
+ */
 
-const childProcess = require('child_process');
-const fs = require('fs');
 const path = require('path');
 
-const ts = require('gulp-typescript');
-const gulp = require('gulp');
-const clean = require('gulp-clean');
+const projectDir = __dirname;
+const tsconfigPath = path.join(projectDir, 'tools/gulp/tsconfig.json');
 
-const { join } = require('path');
-const debug = require('gulp-debug');
-const { promisify } = require('util');
-
-const exec = promisify(childProcess.exec);
-
-/**
- * SETTINGS
- */
-const DIST = join(__dirname, 'dist');
-const LIB = join(__dirname, 'lib');
-const SAMPLE = join(__dirname, 'sample');
-
-const terminus = ts.createProject(join(__dirname, 'tsconfig.json'));
-
-/**
- * UTIL
- */
-function getFolders(dir) {
-  return fs
-    .readdirSync(dir)
-    .filter((file) => fs.statSync(path.join(dir, file)).isDirectory());
-}
-
-const getDirs = (base) => getFolders(base).map((path) => `${base}/${path}`).filter(p => !p.includes('deprecated'));
-
-/**
- * TASK
- */
-
-gulp.task('clean', () => {
-  return gulp
-    .src(
-      [DIST, SAMPLE + '/*/node_modules/@nestjs/terminus', SAMPLE + '/*/dist'],
-      {
-        allowEmpty: true,
-      },
-    )
-    .pipe(clean());
+require('ts-node').register({
+  project: tsconfigPath
 });
 
-gulp.task('move:protos', () => {
-  return gulp
-    .src(join(LIB, './**/*.proto'), { base: LIB })
-    .pipe(debug({ title: 'move:protos' }))
-    .pipe(gulp.dest(join(DIST)));
-});
-
-gulp.task('build', () => {
-  return terminus.src().pipe(terminus()).pipe(gulp.dest(DIST));
-});
-
-gulp.task('move', () => {
-  const directories = getDirs(SAMPLE);
-
-  let stream = gulp.src(['dist/**/*']);
-
-  directories.forEach((dir) => {
-    stream = stream.pipe(gulp.dest(dir + '/node_modules/@nestjs/terminus'));
-  });
-  return stream;
-});
-
-gulp.task('install:sample', async () => {
-  const directories = getDirs(SAMPLE);
-
-  const promises = directories.map((dir) =>
-    exec(`npm install --prefix ${dir}`),
-  );
-
-  return await Promise.all(promises);
-});
-
-gulp.task('build:sample', async () => {
-  const directories = getDirs(SAMPLE);
-
-  const promises = directories.map((dir) =>
-    exec(`npm run build --prefix ${dir}`),
-  );
-
-  return await Promise.all(promises);
-});
-
-/**
- * Aliases
- */
-gulp.task('default', gulp.series(['clean', 'build', 'move:protos']));
-gulp.task('build:app', gulp.series(['default']));
-gulp.task(
-  'build:all',
-  gulp.series([
-    'build:app',
-    'install:sample',
-    'move',
-    'build:sample',
-  ]),
-);
+require('./tools/gulp/gulpfile');
---
+++ tools/gulp/tasks/samples.ts
@@ -0,0 +1,71 @@
+import * as childProcess from 'child_process';
+import * as clc from 'cli-color';
+import * as log from 'fancy-log';
+import { task, series } from 'gulp';
+import { resolve } from 'path';
+import { promisify } from 'util';
+import { samplePath } from '../config';
+import { getDirs } from '../util/task-helpers';
+
+const exec = promisify(childProcess.exec);
+
+async function executeNpmScriptInSamples(
+  script: string,
+  appendScript?: string,
+) {
+  const directories = getDirs(samplePath);
+
+  for await (const dir of directories) {
+    const dirName = dir.replace(resolve(__dirname, '../../../'), '');
+    log.info(`Running ${clc.blue(script)} in ${clc.magenta(dirName)}`);
+    try {
+      const result = await exec(
+        `${script} --prefix ${dir} ${appendScript ? '-- ' + appendScript : ''}`,
+      );
+      log.info(
+        `Finished running ${clc.blue(script)} in ${clc.magenta(dirName)}`,
+      );
+      if (result.stderr) {
+        log.error(result.stderr);
+      }
+      if (result.stdout) {
+        log.error(result.stdout);
+      }
+    } catch (err) {
+      log.error(
+        `Failed running ${clc.blue(script)} in ${clc.magenta(dirName)}`,
+      );
+      if (err.stderr) {
+        log.error(err.stderr);
+      }
+      if (err.stdout) {
+        log.error(err.stdout);
+      }
+      process.exit(1);
+    }
+  }
+}
+
+task('install:samples', async () =>
+  executeNpmScriptInSamples(
+    // 'npm ci --no-audit --no-shrinkwrap --no-optional',
+    'npm install',
+  ),
+);
+task('build:samples', async () => executeNpmScriptInSamples('npm run build'));
+task('test:samples', async () =>
+  executeNpmScriptInSamples('npm run test', '--passWithNoTests'),
+);
+task('test:e2e:samples', async () =>
+  executeNpmScriptInSamples('npm run test:e2e', '--passWithNoTests'),
+);
+
+task(
+  'build:all',
+  series([
+    'build:app',
+    'install:samples',
+    'move',
+    'build:samples',
+  ]),
+);
