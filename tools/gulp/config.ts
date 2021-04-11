fecb75abbf3f4b3155f64f74f7a58f7925c1f6e0
Livio <livio.brunner.lb1@gmail.com>
2021-04-11 21:36:24 +0200

    chore: fix build task

--- tools/gulp/config.ts
+++ tools/gulp/config.ts
@@ -1,8 +1,11 @@
 import { getDirs } from './util/task-helpers';
+import { join } from 'path';
 
 // All paths are related to the base dir
-export const libPath = 'lib';
-export const samplePath = 'sample';
-export const distPath = 'dist';
+export const rootFolder = join(__dirname, '../../');
+export const tsconfig = join(rootFolder, 'tsconfig.json');
+export const libPath = join(rootFolder, 'lib');
+export const samplePath = join(rootFolder, 'sample');
+export const distPath = join(rootFolder, 'dist');
 
 export const libPaths = getDirs(libPath);
