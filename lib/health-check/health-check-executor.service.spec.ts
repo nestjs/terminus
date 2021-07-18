70e36edd61e23e3511d280fb73346b7348213d72
Livio Brunner <livio.brunner.lb1@gmail.com>
2021-07-18 22:07:04 +0200

    refactor: remove deprecated Terminus API
    BREAKING CHANGE: TerminusModule.forRoot{Async) has been removed
    
    In order to migrate, check out: https://docs.nestjs.com/migration-guide#terminus

--- lib/health-check/health-check-executor.service.spec.ts
+++ lib/health-check/health-check-executor.service.spec.ts
@@ -2,7 +2,7 @@ import { Test } from '@nestjs/testing';
 import { HealthCheckExecutor } from './health-check-executor.service';
 import { HealthIndicatorResult } from '../health-indicator';
 import { HealthCheckResult } from './health-check-result.interface';
-import { HealthCheckError } from '@godaddy/terminus';
+import { HealthCheckError } from '../health-check/health-check.error';
 
 const healthyCheck = async (): Promise<HealthIndicatorResult> => {
   return {
@@ -96,49 +96,6 @@ describe('HealthCheckExecutorService', () => {
           },
         },
       });
-    });
-  });
-
-  describe('executeDeprecated', () => {
-    it('should return a result object without errors', async () => {
-      const result = await healthCheckExecutor.executeDeprecated([
-        () => healthyCheck(),
-      ]);
-      expect(result).toEqual<HealthIndicatorResult>({
-        healthy: {
-          status: 'up',
-        },
-      });
-    });
-
-    it('should return a result object with errors', async () => {
-      try {
-        await healthCheckExecutor.executeDeprecated([() => unhealthyCheck()]);
-      } catch (error) {
-        expect(error.causes).toEqual<HealthIndicatorResult>({
-          unhealthy: {
-            status: 'down',
-          },
-        });
-      }
-    });
-
-    it('should return a result object with mixed errors', async () => {
-      try {
-        await healthCheckExecutor.executeDeprecated([
-          () => healthyCheck(),
-          () => unhealthyCheck(),
-        ]);
-      } catch (error) {
-        expect(error.causes).toEqual<HealthIndicatorResult>({
-          healthy: {
-            status: 'up',
-          },
-          unhealthy: {
-            status: 'down',
-          },
-        });
-      }
     });
   });
 });
--- lib/terminus.module.ts
+++ lib/terminus.module.ts
@@ -1,10 +1,5 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import {
-  TerminusModuleOptions,
-  TerminusModuleAsyncOptions,
-} from './terminus-module-options.interface';
-import { TerminusCoreModule } from './terminus-core.module';
 import { DiskusageLibProvider } from './health-indicator/disk/diskusage-lib.provider';
 import { HEALTH_INDICATORS } from './health-indicator/health-indicators.provider';
 import { HealthCheckService } from './health-check';
@@ -26,27 +21,4 @@ import { HealthCheckExecutor } from './health-check/health-check-executor.servic
   ],
   exports: [HealthCheckService, ...HEALTH_INDICATORS],
 })
-export class TerminusModule {
-  /**
-   * Bootstraps the Terminus Module synchronously
-   * @param options The options for the Terminus Module
-   * @deprecated
-   */
-  static forRoot(options?: TerminusModuleOptions): DynamicModule {
-    return {
-      module: TerminusModule,
-      imports: [TerminusCoreModule.forRoot(options)],
-    };
-  }
-
-  /**
-   * Bootstrap the Terminus Module asynchronously
-   * @param options The options for the Terminus module
-   */
-  static forRootAsync(options: TerminusModuleAsyncOptions): DynamicModule {
-    return {
-      module: TerminusModule,
-      imports: [TerminusCoreModule.forRootAsync(options)],
-    };
-  }
-}
+export class TerminusModule {}
