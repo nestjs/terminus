f430bce586d299f7e974600eeec6e48dc7197e68
Livio Brunner <livio.brunner.lb1@gmail.com>
2021-07-18 22:09:30 +0200

    chore: remove deprecated samples folder

--- sample/deprecated/000-dogs-app/src/dog/dog.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DogService } from './dog.service';
-import { DogHealthIndicator } from './dog.health';
-
-@Module({
-  providers: [DogService, DogHealthIndicator],
-  exports: [DogHealthIndicator],
-})
-export class DogModule {}
