aecbe4b66881b74542279b1ed7a9864db1e0831e
Jason Watson <jwatson@breachquest.com>
2022-06-24 14:16:42 +0100

    feat(health-indicator): add mikro-orm health indicator

---
+++ e2e/health-checks/mikro-orm.health.e2e-spec.ts
@@ -0,0 +1,53 @@
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { bootstrapTestingModule, DynamicHealthEndpointFn } from '../helper';
+
+describe('MikroOrmHealthIndicator', () => {
+  let app: INestApplication;
+  let setHealthEndpoint: DynamicHealthEndpointFn;
+
+  beforeEach(
+    () =>
+      (setHealthEndpoint =
+        bootstrapTestingModule().withMikroOrm().setHealthEndpoint),
+  );
+
+  describe('#pingCheck', () => {
+    it('should check if the mikroOrm is available', async () => {
+      app = await setHealthEndpoint(({ healthCheck, mikroOrm }) =>
+        healthCheck.check([async () => mikroOrm.pingCheck('mikroOrm')]),
+      ).start();
+      const details = { mikroOrm: { status: 'up' } };
+      return request(app.getHttpServer()).get('/health').expect(200).expect({
+        status: 'ok',
+        info: details,
+        error: {},
+        details,
+      });
+    });
+
+    it('should throw an error if runs into timeout error', async () => {
+      app = await setHealthEndpoint(({ healthCheck, mikroOrm }) =>
+        healthCheck.check([
+          async () => mikroOrm.pingCheck('mikroOrm', { timeout: 1 }),
+        ]),
+      ).start();
+
+      const details = {
+        mikroOrm: {
+          status: 'down',
+          message: 'timeout of 1ms exceeded',
+        },
+      };
+
+      return request(app.getHttpServer()).get('/health').expect(503).expect({
+        status: 'error',
+        info: {},
+        error: details,
+        details,
+      });
+    });
+  });
+
+  afterEach(async () => await app.close());
+});
