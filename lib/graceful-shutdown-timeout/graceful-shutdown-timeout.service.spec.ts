dd71df6f4ea7b35dac0d8b5f727be503eff97a5c
Francois LP <francois.le.pape@gmail.com>
2023-11-22 23:44:29 +0100

    feat: add graceful-shutdown timeout service with tests
    set default option to 0ms
    
    Related issue: https://github.com/nestjs/terminus/issues/2421

---
+++ lib/graceful-shutdown-timeout/graceful-shutdown-timeout.service.spec.ts
@@ -0,0 +1,47 @@
+import { Test } from '@nestjs/testing';
+import { LoggerService } from '@nestjs/common';
+import {
+  GracefulShutdownTimeoutService,
+  TERMINUS_GRACEFUL_SHUTDOWN_TIMEOUT,
+} from './graceful-shutdown-timeout.service';
+import { TERMINUS_LOGGER } from '../health-check/logger/logger.provider';
+import { sleep } from '../utils';
+
+jest.mock('../utils', () => ({
+  sleep: jest.fn(),
+}));
+
+const loggerMock: Partial<LoggerService> = {
+  log: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  debug: jest.fn(),
+};
+
+describe('GracefulShutdownTimeoutService', () => {
+  let service: GracefulShutdownTimeoutService;
+  let logger: LoggerService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        GracefulShutdownTimeoutService,
+        { provide: TERMINUS_LOGGER, useValue: loggerMock },
+        { provide: TERMINUS_GRACEFUL_SHUTDOWN_TIMEOUT, useValue: 1000 },
+      ],
+    }).compile();
+
+    logger = module.get(TERMINUS_LOGGER);
+    service = module.get(GracefulShutdownTimeoutService);
+  });
+
+  it('should not trigger sleep if signal is not SIGTERM', async () => {
+    await service.beforeApplicationShutdown('SIGINT');
+    expect(sleep).not.toHaveBeenCalled();
+  });
+
+  it('should trigger sleep if signal is SIGTERM', async () => {
+    await service.beforeApplicationShutdown('SIGTERM');
+    expect(sleep).toHaveBeenCalledWith(1000);
+  });
+});
--- lib/terminus-options.interface.ts
+++ lib/terminus-options.interface.ts
@@ -2,7 +2,15 @@ import { type LoggerService, type Type } from '@nestjs/common';
 
 export type ErrorLogStyle = 'pretty' | 'json';
 
+/**
+ * The Terminus module options
+ *
+ * errorLogStyle: The style of the error logger. Either 'pretty' or 'json'. Default to 'json'.
+ * logger: The logger to use. Either default logger or your own.
+ * gracefulShutdownTimeoutMs: The timeout to wait in ms before the application shuts down. Default to 0ms.
+ */
 export interface TerminusModuleOptions {
   errorLogStyle?: ErrorLogStyle;
   logger?: Type<LoggerService> | boolean;
+  gracefulShutdownTimeoutMs?: number;
 }
